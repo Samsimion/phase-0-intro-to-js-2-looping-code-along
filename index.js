83c7432ea2166e3e07b98fb1c8769083ad94ad61
simion samuel <samuel.simion@student.moringaschool.com>
2025-03-14 17:27:48 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+let name = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(name){
+    let msg=[];
+    
+    for(let i =0; i<name.length; i++){
+        msg[i]= `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+
+    }
+    return msg;
+}
+function countDown(num){
+    while(num >= 0){
+        console.log(num) ;
+        num--;  
+    }
+
+}
+console.log(countDown(10));
+
